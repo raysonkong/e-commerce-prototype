5747b7203b496efae414768ed398ade8a16524ac
Rayson Kong <kong.rayson@gmail.com>
2021-03-21 11:08:44 +0800

    successfully separated menu-item into its own component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -8,41 +8,10 @@ const HomePage = () => {
       <div className="directory-menu">
 
         <MenuItem title={'Special Item'}/>
-        <div className="menu-item">
-
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Sneakers</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Jackets</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
+        <MenuItem title={'Special Item'}/>
+        <MenuItem title={'Special Item'}/>
+        <MenuItem title={'Special Item'}/>
+        <MenuItem title={'Special Item'}/>
 
       </div>
     </div>
