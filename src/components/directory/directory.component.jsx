7d8a52ff8d94573ae4f959ca5a7584e8f5f9ae5e
Rayson Kong <kong.rayson@gmail.com>
2021-03-22 11:51:28 +0800

    added product data file and iterated over it

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,18 +7,51 @@ class Directory extends React.Component {
         super();
 
         this.state = {
-            sections: []
+            sections: [
+             
+              {
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1,
+                linkUrl: 'shop/hats'
+              },
+              {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: 'shop/jackets'
+              },
+              {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: 'shop/sneakers'
+              },
+              {
+                title: 'womens',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                size: 'large',
+                id: 4,
+                linkUrl: 'shop/womens'
+              },
+              {
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                size: 'large',
+                id: 5,
+                linkUrl: 'shop/mens'
+              }
+            ]
         }
     }
     
     render() {
+      const itemsArray = this.state.sections.map(item => {
+        return <MenuItem title={item.title}/>
+      })
         return (
             <div className="directory-menu">
-              <MenuItem title={'Special Item'}/>
-              <MenuItem title={'Special Item'}/>
-              <MenuItem title={'Special Item'}/>
-              <MenuItem title={'Special Item'}/>
-              <MenuItem title={'Special Item'}/>
+              {itemsArray}
             </div>
         )
   }
