412981cd246ba52b8a4d53c247c5207a8880a794
Rayson Kong <kong.rayson@gmail.com>
2021-03-25 11:59:22 +0800

    add routing with withRouter()

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,26 +12,26 @@ class Directory extends React.Component {
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
-                linkUrl: 'shop/hats'
+                linkUrl: 'hats'
               },
               {
                 title: 'jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                 id: 2,
-                linkUrl: 'shop/jackets'
+                linkUrl: 'jackets'
               },
               {
                 title: 'sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id: 3,
-                linkUrl: 'shop/sneakers'
+                linkUrl: 'sneakers'
               },
               {
                 title: 'womens',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 size: 'large',
                 id: 4,
-                linkUrl: 'shop/womens',
+                linkUrl: 'womens',
                 size: 'large'
               },
               {
@@ -39,7 +39,7 @@ class Directory extends React.Component {
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 size: 'large',
                 id: 5,
-                linkUrl: 'shop/mens',
+                linkUrl: 'mens',
                 size: 'large'
               }
             ]
@@ -55,6 +55,7 @@ class Directory extends React.Component {
             imageUrl={item.imageUrl}
             key={item.id}
             size={item.size}
+            linkUrl={item.linkUrl}
           />
         )
       })
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({title, imageUrl, size, linkUrl, history, match }) => {
     return (
-      <div className={`${size} menu-item`}>
+      <div 
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+
+      >
         <div 
           className="background-image"
           style={{backgroundImage: `url(${imageUrl})`}}
@@ -12,11 +17,12 @@ const MenuItem = ({title, imageUrl, size}) => {
 
         <div className="content">
           <h1 className="title">{title.toUpperCase()}</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span className="subtitle">match.url is: {`${match.url}`}  SHOP NOW</span>
+
         </div>
       </div>
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
 
